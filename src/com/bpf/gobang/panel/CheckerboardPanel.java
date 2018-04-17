41adfe6f6f63c24f8900306e1be5afbcd02fface
xiaobai1226 <baipengfeii@qq.com>
2018-04-17 08:36:57 +0800

    更换棋子图片，增加双缓冲技术防止屏幕闪烁

--- src/com/bpf/gobang/panel/CheckerboardPanel.java
+++ src/com/bpf/gobang/panel/CheckerboardPanel.java
@@ -1,6 +1,7 @@
 package com.bpf.gobang.panel;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -53,15 +54,19 @@ public class CheckerboardPanel extends JPanel{
 	@Override
 	public void paint(Graphics g) {
 		try {
+			//利用双缓冲技术，防止屏幕闪烁
+			BufferedImage bufferImage = new BufferedImage(800,830,BufferedImage.TYPE_INT_ARGB);
+			Graphics graphics = bufferImage.createGraphics();
+			
 			//添加棋盘图片
-			g.drawImage(ImageIO.read(new File(checkerboard.getCHECKERBOARD_IMAGE_URL())), 0, 0, this);
+			graphics.drawImage(ImageIO.read(new File(checkerboard.getCHECKERBOARD_IMAGE_URL())), 0, 0, this);
 			//添加光标
 			//只有在棋盘范围内时，才显示此选择框
 			if(checkerboard.getCursor_position()[0] != -1 && checkerboard.getCursor_position()[1] != -1) {
 				cursorX = CheckerboardAlgorithm.calculationPositionByCoordinate(checkerboard.getCursor_position()[0]);
 				cursorY = CheckerboardAlgorithm.calculationPositionByCoordinate(checkerboard.getCursor_position()[1]);
 				
-				g.drawImage(ImageIO.read(new File(checkerboard.getCURSOR_IMAGE_URL())), cursorX, cursorY, this);
+				graphics.drawImage(ImageIO.read(new File(checkerboard.getCURSOR_IMAGE_URL())), cursorX, cursorY, this);
 			}
 			
 			//获取当前棋盘信息
@@ -70,18 +75,20 @@ public class CheckerboardPanel extends JPanel{
 			for(int i = 0; i < checkerboard_situation.length; i++) {
 				for(int j = 0; j < checkerboard_situation.length; j++) {
 					if(checkerboard_situation[i][j] == 1) {
-						g.drawImage(ImageIO.read(new File(checkerboard.getBLACK_CHESS_PIECES_IMAGE_URL())), //棋子图片
+						graphics.drawImage(ImageIO.read(new File(checkerboard.getBLACK_CHESS_PIECES_IMAGE_URL())), //棋子图片
 								CheckerboardAlgorithm.calculationCoordinateByIndex(i), //棋子横坐标
 								CheckerboardAlgorithm.calculationCoordinateByIndex(j), //棋子纵坐标
-								40,40,this);
+								this);
 					}else if(checkerboard_situation[i][j] == 2) {
-						g.drawImage(ImageIO.read(new File(checkerboard.getWHITE_CHESS_PIECES_IMAGE_URL())), 
+						graphics.drawImage(ImageIO.read(new File(checkerboard.getWHITE_CHESS_PIECES_IMAGE_URL())), 
 								CheckerboardAlgorithm.calculationCoordinateByIndex(i), 
 								CheckerboardAlgorithm.calculationCoordinateByIndex(j),
-								40,40,this);
+								this);
 					}
 				}
 			}
+			
+			g.drawImage(bufferImage, 0, 0, this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/com/bpf/gobang/panel/MenuPanel.java
+++ src/com/bpf/gobang/panel/MenuPanel.java
@@ -1,6 +1,7 @@
 package com.bpf.gobang.panel;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -48,46 +49,51 @@ public class MenuPanel extends JPanel{
 	@Override
 	public void paint(Graphics g) {
 		try {
+			//利用双缓冲技术，防止屏幕闪烁
+			BufferedImage bufferImage = new BufferedImage(534,830,BufferedImage.TYPE_INT_ARGB);
+			Graphics graphics = bufferImage.createGraphics();
+			
 			//添加菜单背景
-			g.drawImage(ImageIO.read(new File(menu.getMENU_BACKGROUND_IMAGE_URL())), 0, 0, this);
+			graphics.drawImage(ImageIO.read(new File(menu.getMENU_BACKGROUND_IMAGE_URL())), 0, 0, this);
 			
 			//添加联机模式按钮
 			if(menu.getCURRENT_BUTTON().equals("play_online")) {
-				g.drawImage(ImageIO.read(new File(menu.getPLAY_ONLINE_IMAGE_URL())), 157, 247, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getPLAY_ONLINE_IMAGE_URL())), 157, 247, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getPLAY_ONLINE_IMAGE_URL())), 167, 250, 200, 55, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getPLAY_ONLINE_IMAGE_URL())), 167, 250, 200, 55, this);
 			}
 			//添加人机对弈按钮
 			if(menu.getCURRENT_BUTTON().equals("player_vs_computer")) {
-				g.drawImage(ImageIO.read(new File(menu.getPLAYER_VS_COMPUTER_IMAGE_URL())), 157, 357, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getPLAYER_VS_COMPUTER_IMAGE_URL())), 157, 357, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getPLAYER_VS_COMPUTER_IMAGE_URL())), 167, 360, 200, 55, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getPLAYER_VS_COMPUTER_IMAGE_URL())), 167, 360, 200, 55, this);
 			}
 			//添加双人对弈按钮
 			if(menu.getCURRENT_BUTTON().equals("two_player_game")) {
-				g.drawImage(ImageIO.read(new File(menu.getTWO_PLAYER_GAME_IMAGE_URL())), 157, 467, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getTWO_PLAYER_GAME_IMAGE_URL())), 157, 467, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getTWO_PLAYER_GAME_IMAGE_URL())), 167, 470, 200, 55, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getTWO_PLAYER_GAME_IMAGE_URL())), 167, 470, 200, 55, this);
 			}
 			//添加关于按钮
 			if(menu.getCURRENT_BUTTON().equals("about")) {
-				g.drawImage(ImageIO.read(new File(menu.getABOUT_IMAGE_URL())), 157, 577, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getABOUT_IMAGE_URL())), 157, 577, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getABOUT_IMAGE_URL())), 167, 580, 200, 55, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getABOUT_IMAGE_URL())), 167, 580, 200, 55, this);
 			}
 			//添加战绩按钮
 			if(menu.getCURRENT_BUTTON().equals("record")) {
-				g.drawImage(ImageIO.read(new File(menu.getBIG_RECORD_IMAGE_URL())), 80, 16, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getBIG_RECORD_IMAGE_URL())), 80, 16, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getRECORD_IMAGE_URL())), 85, 20, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getRECORD_IMAGE_URL())), 85, 20, this);
 			}
 			//添加规则按钮
 			if(menu.getCURRENT_BUTTON().equals("rules")) {
-				g.drawImage(ImageIO.read(new File(menu.getBIG_GAME_RULES_IMAGE_URL())), 310, 27, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getBIG_GAME_RULES_IMAGE_URL())), 310, 27, this);
 			}else {
-				g.drawImage(ImageIO.read(new File(menu.getGAME_RULES_IMAGE_URL())), 315, 30, this);
+				graphics.drawImage(ImageIO.read(new File(menu.getGAME_RULES_IMAGE_URL())), 315, 30, this);
 			}
 			
+			g.drawImage(bufferImage, 0, 0, this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
