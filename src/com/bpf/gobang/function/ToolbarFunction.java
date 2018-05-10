07d9508708f92077af03d3cc42d5aad85f91b4b2
xiaobai1226 <baipengfeii@qq.com>
2018-05-10 10:49:11 +0800

    修改人机对战，电脑先行悔棋时的bug，并且上传git忽略文件

--- src/com/bpf/gobang/function/ToolbarFunction.java
+++ src/com/bpf/gobang/function/ToolbarFunction.java
@@ -23,59 +23,65 @@ public class ToolbarFunction {
 	public static void regret() {
 		//根据当前页面选择使用的棋盘属性
 		Checkerboard checkerboard = Checkerboard.getCheckerboard(Common.getCommon().getCurrent_page());
-		//将当前状态置为false
-		Common.getCommon().setCurrent_status(false);
 		
-		Thread t = new Thread() {
-			public void run() {
-				//获取当期棋局记录的长度
-				int length = checkerboard.getChessRecord().size();
-				int max = 0;
-				if(Common.getCommon().getCurrent_page().equals(Common.COMPUTER_VS_PLAYER)) {
-					max = 2;
-				}else if(Common.getCommon().getCurrent_page().equals(Common.TWOPLAYER)) {
-					max = 1;
-				}
-				int i = 0;
-				while(i < max && length > 0) {
-					//获取当期棋局，最后下的棋子的坐标
-					int[] last = checkerboard.getChessRecord().get(length - 1);
-					//清除当期棋局，最后下的棋子
-					checkerboard.getCheckerboardSituation()[last[0]][last[1]] = 0;
-					//清除当前棋局记录，最后下的棋子
-					checkerboard.getChessRecord().remove(length - 1);
-					//
-					checkerboard.setCurrent_chess_piece(!checkerboard.getCurrent_chess_piece());
-					
-					//
-					CheckerboardFunction.restoreWin();
-					//
-					CheckerboardFunction.restoreTable();
-					
-					//重绘该窗体
-					CheckerboardFrame.getCheckerboardFrame().repaint();
-					
-					i++;
-					length--;
-					//睡眠500毫秒
-					try {
-						if(length > 0 && i < 2) {
-							Thread.sleep(500);
-						}
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
+		if(!(Common.getCommon().getCurrent_page().equals(Common.COMPUTER_VS_PLAYER) &&
+				checkerboard.getFirst_player() && checkerboard.getChessRecord().size() == 1)) {
+			
+			//将当前状态置为false
+			Common.getCommon().setCurrent_status(false);
+			
+			Thread t = new Thread() {
+				public void run() {
+					//获取当期棋局记录的长度
+					int length = checkerboard.getChessRecord().size();
+					int max = 0;
+					if(Common.getCommon().getCurrent_page().equals(Common.COMPUTER_VS_PLAYER)) {
+						max = 2;
+					}else if(Common.getCommon().getCurrent_page().equals(Common.TWOPLAYER)) {
+						max = 1;
 					}
+					int i = 0;
+					while(i < max && length > 0) {
+						//获取当期棋局，最后下的棋子的坐标
+						int[] last = checkerboard.getChessRecord().get(length - 1);
+						//清除当期棋局，最后下的棋子
+						checkerboard.getCheckerboardSituation()[last[0]][last[1]] = 0;
+						//清除当前棋局记录，最后下的棋子
+						checkerboard.getChessRecord().remove(length - 1);
+						//
+						checkerboard.setCurrent_chess_piece(!checkerboard.getCurrent_chess_piece());
+						
+						//
+						CheckerboardFunction.restoreWin();
+						//
+						CheckerboardFunction.restoreTable();
+						
+						//重绘该窗体
+						CheckerboardFrame.getCheckerboardFrame().repaint();
+						
+						i++;
+						length--;
+						//睡眠500毫秒
+						try {
+							if(length > 0 && i < 2) {
+								Thread.sleep(500);
+							}
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+					}
+					
+					//如果棋子记录数为0，将当前棋子颜色置为黑色
+					if(length == 0) {
+						checkerboard.setCurrent_chess_piece(false);
+					}
+					//将当前状态置为true
+					Common.getCommon().setCurrent_status(true);
 				}
-				
-				//如果棋子记录数为0，将当前棋子颜色置为黑色
-				if(length == 0) {
-					checkerboard.setCurrent_chess_piece(false);
-				}
-				//将当前状态置为true
-				Common.getCommon().setCurrent_status(true);
-			}
-		};
-		t.start();
+			};
+			//启动线程
+			t.start();
+		}
 	}
 	
 	/**
